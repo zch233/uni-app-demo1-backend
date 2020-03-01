b579e99e5f561fc266a3d7509edd71632d342e09
zhangchao <529743595@qq.com>
2020-03-01 19:12:30 +0800

    Fix SearchTime

--- src/views/Statistics/Order/index.vue
+++ src/views/Statistics/Order/index.vue
@@ -72,6 +72,7 @@
     },
     methods: {
       async getOrderList () {
+        Object.keys(this.searchForm).map(v => (this.searchForm[v] = this.searchForm[v] / 1000))
         const data = await getOrderList({ page_size: this.pageSize, current_page: this.currentPage, ...this.searchForm })
         this.tableData = [data]
         this.total = data.total_num
